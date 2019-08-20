36696fe47b3eb837381299176bfabdcc023cf469
placidseven <54321928+placidseven@users.noreply.github.com>
2019-08-20 11:09:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $("#labels div:even").addClass("even");
+  $(".gender div:even").addClass("even");
+  $(".legal div:even").addClass("even");
+  $("#selector div:even").addClass("even");
+  
+  $('#submit').click( function() { alert('Thank you for your participation'); });
+  
+});
